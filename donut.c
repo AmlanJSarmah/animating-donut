08afe69cabb6968bcf8b828a9799daf2da633ed9
A-J-Sarmah <ajsarmah@tutanota.com>
2022-05-29 14:59:26 +0530

    Setup code

--- donut.c
+++ donut.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <math.h>
-#include <string.h>
-#include <unistd.h>
 
 int main()
-{
-  //angle of rotation around the x and z axis
-  float A=0, B=0;
-  float i=0,j=0;
-  int k=0;
-  float z_buffer[1760];
-  char output_buffer[1760];
-  printf("\x1b[2J"); //creates a spacing on the screen
+{  
   return 0;
 }
